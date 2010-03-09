95efa4ee0e6f52c0511eb21c4d384a8121542204
Peter Dodds <peter.dodds@gmail.com>
2010-03-09 12:31:20 +0000

    tweeked event functions and fixed dos line endings in pnm.py

--- mkwavevent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#mkdir $1 && echo "# keep me" > $1/__init__.py
-if [ -e pywav/events/$1.py ] ; then
-	echo "Event already exists"
-else 
-	echo -e "\ndef e(app, maker, data):\n\tapp.log.debug(\"$1 fired\")\n\treturn True" > pywav/events/$1.py
-	echo "pywav/events/$1.py"
-fi
--- pnm/events/_scripts/ping.py
+++ pnm/events/_scripts/ping.py
@@ -1,6 +1,8 @@
 ## Event function.
 #  @param eman - event manager
 #  @param data - information passed
+#  @return True if the event is successful
 def e (eman, data):
 	print "ping"
+	
 	return True
