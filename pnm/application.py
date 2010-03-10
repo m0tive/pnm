5fb12cdf0aabb3a4314e3ee03e175ebb97cada05
Peter Dodds <peter.dodds@gmail.com>
2010-03-10 17:02:37 +0000

    split application and basic singleton application into seperate classes

--- pnm/application.py
+++ pnm/application.py
@@ -1,28 +1,13 @@
 
+from singletonApp import SingletonApp
 from logger import Log
-from events import EventManager
 
-## Singleton application class
-#
-#  Singleton pattern taken from post on Stackoverflow.com by "modi"
-#  http://stackoverflow.com/questions/42558/python-and-the-singleton-pattern/1810367#1810367
-#  Accessed: 9th March 2010
-class Application (object):
-	__inst = None
-	
-	def __new__(cls, **kwargs):
-		if not cls.__inst:
-			cls.__inst = super(Application,cls).__new__(cls)
-			## Initialise log and send info message
-			Log(level="DEBUG").info("Application instance created")
-			cls.__inst.__setup()
-		elif kwargs:
-			Log().warning("Application arguments not used after creation")
-		return cls.__inst
-		
-	def __setup(self):
-		self.eventManager = EventManager()
-		Log().info("Application setup")
+## Application class
+class Application (SingletonApp):
+	def _setup(self):
+		super(Application,self)._setup()
+		Log().debug("Application setup")
+		self.eventManager.hook("application_setup")
 		
 	def start(self):
 		self.eventManager.hook("application_start")
